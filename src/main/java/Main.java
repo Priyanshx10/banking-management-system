3c0a7fb317307e424529adef0fb80eee27111b1d
Priyansh Yadav <priyanshyadav1012@gmail.com>
2026-09-07 03:01:15 +0530

    feat: create bank account model and application entry point

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Banking Management System!");
+        BankAccount account = new BankAccount(101201, "Priyansh Yadav", 2000.0);
+        System.out.println("Bank account object created successfully!");
+    }
+}
